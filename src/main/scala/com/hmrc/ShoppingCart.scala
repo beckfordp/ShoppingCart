1e9b852a2be3902c3e41f5afa117ab3a8acc9649
beckfordp <beckfordp@btinternet.com>
2020-10-22 14:29:08 +0100

    Step 2 complete

--- src/main/scala/com/hmrc/ShoppingCart.scala
+++ src/main/scala/com/hmrc/ShoppingCart.scala
@@ -1,8 +1,21 @@
 package com.hmrc
 
+import com.hmrc.Item.{Apple, Orange}
+
 case class ShoppingCart(items: List[Item] = Nil) {
   def total: Int = {
-    items.map(_.price).sum
+
+    val apples = items.filter(_ == Apple).sliding(2,2).flatMap  {
+      case List(Apple, Apple) => List(Apple)
+      case apples => apples
+    }
+
+    val oranges = items.filter(_ == Orange).sliding(2,3).flatMap {
+      case List(Orange, Orange, Orange) => List(Orange, Orange)
+      case oranges => oranges
+    }
+
+    (apples ++ oranges).map(_.price).sum
   }
 
   def add(item: Item): ShoppingCart = ShoppingCart(item :: items)
--- src/test/scala/com/hmrc/ShoppingCartSpec.scala
+++ src/test/scala/com/hmrc/ShoppingCartSpec.scala
@@ -12,19 +12,34 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
     
      cart.items should  be (List(Apple, Orange).reverse)
   }
-
-  it should "calculate total" in {
-    val cart =  ShoppingCart()
-      .add(Apple)
-      .add(Apple)
-      .add(Orange)
-      .add(Apple)
-
-    cart.total should be (205)
-  }
-
+  
   it should "add from list of item names"  in {
     val cart = ShoppingCart.fromItemNames(List("Apple", "Orange"))
     cart.items should be(List(Apple, Orange))
+  }
+
+  it should "calculate two for the price of one on Apples" in {
+    val cart = ShoppingCart.fromItemNames(List("Apple", "Apple"))
+    cart.total should be (60)
+  }
+
+  it should "calculate three for the price of two Oranges" in {
+    val cart = ShoppingCart.fromItemNames(List("Orange", "Orange", "Orange"))
+    cart.total should be (50)
+  }
+
+  it should "calculate the correct price for 4 Oranges" in {
+    val cart = ShoppingCart.fromItemNames(List("Orange", "Orange", "Orange", "Orange"))
+    cart.total should be (75)
+  }
+
+  it should "calculate the correct price for 3 Apples" in {
+    val cart = ShoppingCart.fromItemNames(List("Apple", "Apple", "Apple"))
+    cart.total should be (120)
+  }
+
+  it should "calculate the correct price for a mixed basket" in {
+    val cart = ShoppingCart.fromItemNames(List("Apple", "Orange", "Orange", "Apple", "Orange", "Apple", "Orange"))
+    cart.total should be (195)
   }
 }
